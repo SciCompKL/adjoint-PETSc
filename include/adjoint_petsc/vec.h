57f6b9be66fdc2472fb1f25c0c22a9535d1a4f7f
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2025-04-10 16:37:51 +0200

    Added license headers.

--- include/adjoint_petsc/vec.h
+++ include/adjoint_petsc/vec.h
@@ -1,3 +1,31 @@
+/*
+ * adjoint-PETSc
+ *
+ * Copyright (C) 2025 Chair for Scientific Computing (SciComp), University of Kaiserslautern-Landau
+ * Homepage: http://scicomp.rptu.de
+ * Contact:  Prof. Nicolas R. Gauger (codi@scicomp.uni-kl.de)
+ *
+ * Lead developers: Max Sagebaum (SciComp, University of Kaiserslautern-Landau)
+ *
+ * This file is part of adjoint-PETSc (GITHUB_LINK).
+ *
+ * adjoint-PETSc is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * adjoint-PETSc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU
+ * Lesser General Public License along with adjoint-PETSc.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Max Sagebaum (SciComp, University of Kaiserslautern-Landau)
+ */
+
 #pragma once
 
 #include <petscvec.h>
